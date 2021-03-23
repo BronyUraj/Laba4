fc3840ab86460fca93d6c2583c446edf6580d7f8
BronyUraj <ura.gudimov@gmail.com>
2021-03-23 14:12:46 +0300

    Вызов в мейне.

--- Laba4/main.cpp
+++ Laba4/main.cpp
@@ -1,16 +1,12 @@
 #include "Complex.h"
+#include "UsersDefined.h"
 #include <iostream>
 #define PI 3.14159265
-
-
 using namespace std;
 
 int main() {
-	Complex comp1 = Complex(10, PI / 10);
-	Complex comp2 = Complex(1, PI / 12);
-	cout << comp1.Re() << " + " << comp1.Im() << "i" << endl;
-	cout << comp2.Re() << " + " << comp2.Im() << "i" << endl;
-	Complex sumcomp = comp1 / comp2;
-	cout << sumcomp.Re() << " + " << sumcomp.Im() << "i" << endl;
+	Complex arg = Complex(1.2, PI / 3);
+	Complex result = y(arg);
+	cout << result.Re() << " + (" << result.Im() << ")i" << endl;
 	return 0;
 }
